a9d585585647991f80d7bfb540f4d775a3698f6e
Marina <mara.berezin@gmail.com>
2021-11-06 15:27:46 +0000

    forth test, function checks a,b and c

--- index.test.js
+++ index.test.js
@@ -1,7 +1,8 @@
 
 function check(a, b, c) {
     return (a * a === b * b + c * c)
-        || (b * b === a * a + c * c) ?
+        || (b * b === a * a + c * c)
+        || (c * c === a * a + b * b) ?
         "correct" : "incorrect"
 };
 
@@ -16,7 +17,7 @@ describe("right-angle triangle?", () => {
         expect(result).toBe("correct");
     });
 
-    it("should work", () => {
+    it("should not work", () => {
         const a = 4;
         const b = 4;
         const c = 3;
@@ -30,6 +31,16 @@ describe("right-angle triangle?", () => {
         const a = 4;
         const b = 5;
         const c = 3;
+
+        const result = check(a, b, c)
+
+        expect(result).toBe("correct");
+    });
+
+    it("should work", () => {
+        const a = 3;
+        const b = 4;
+        const c = 5;
 
         const result = check(a, b, c)
 
